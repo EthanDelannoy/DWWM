1e420fe1e6737d87b7e738b4fab6b38e0ae6e559
EthanDelannoy <ethan.delannoy12@gmail.com>
2024-04-02 16:22:34 +0200

    02-04-24

---
+++ HTML CSS/10 - JS/js.js
@@ -0,0 +1,75 @@
+let nombre1 = 5;
+let nombre2 = 3;
+
+let resultatAddition = nombre1 +  nombre2
+
+console.log(resultatAddition);
+
+let nombre3 = 10;
+let nombre4 = 7;
+
+let resultatSoustraction = nombre3 -  nombre4
+
+console.log(resultatSoustraction);
+
+let nombre5 = 4;
+let nombre6 = 6;
+
+let resultatMultiplication = nombre5 *  nombre6
+
+console.log(resultatMultiplication);
+
+let	prénom = "John";
+let	nom = "doe";
+
+console.log(prénom + " " + nom);
+
+
+let sujet = "Le chat";
+let verbe = " mange";
+let objet = " la souris";
+
+let phrase = sujet + verbe + objet;
+
+console.log(phrase);
+
+let tableau = [1, 2, 3, 4, 5]
+tableau.forEach(element => {
+    console.log(element);
+});
+
+let tableau2 = []
+
+tableau2.push(10)
+tableau2.push(20)
+tableau2.push(30)
+
+console.log(tableau2);
+
+tableau2.shift(0)
+
+console.log(tableau2);
+
+let personne = {
+	nom: 'Alice',
+	age: 25,
+	ville: 'Paris' 
+	};
+
+    console.log(personne);
+
+    let personne2 = {
+        solde: '1000',
+        titulaire: 'John Doe',
+        };
+    
+    console.log(personne2);
+
+    let solde = 1000
+    let addsolde = 500
+
+    let newsolde = solde + addsolde
+
+    console.log(personne2 + newsolde);
+
+
