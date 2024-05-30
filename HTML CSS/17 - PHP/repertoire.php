18d98370d5548e5314f4edf24e2a1f847179d181
EthanDelannoy <ethan.delannoy12@gmail.com>
2024-05-30 09:34:36 +0200

    30-05-24

--- HTML CSS/17 - PHP/repertoire.php
+++ HTML CSS/17 - PHP/repertoire.php
@@ -1,7 +1,9 @@
 <?php
 
+// Tableau vide pour pouvoir y mettre des contacts plus tard
 $repertoire = [];
 
+// Fonction pour ajouter des contacts
 function PushTabl($nom, $numero) {
     global $repertoire;
     array_push($repertoire, [$nom, $numero]);
@@ -10,6 +12,7 @@ function PushTabl($nom, $numero) {
     echo "**********************************************\n";
 }
 
+// Fonction pour rechercher un contact
 function recherche($nom) {
     global $repertoire;
     foreach ($repertoire as $contact) {
@@ -20,6 +23,7 @@ function recherche($nom) {
     return "Contact non trouvé \n";
 }
 
+// Fonction pour voir tout les contacts
 function toutVoir() {
     global $repertoire;
     if (count($repertoire) === 0) {
@@ -47,7 +51,7 @@ function toutVoir() {
 
 
 
-
+// Boucle do While pour rentrer au moin 1 fois dans la boucle
 do {
 $choix1 = 1;
 $choix2 = 2;
@@ -57,14 +61,17 @@ echo "Vous souhaitez :\n\n $choix1 : Ajouter un nouveau contact\n $choix2 : Rech
 
 $choix = readline("Que voulez-vous faire ? ");
 
+// Switch pour traiter le choix que l'utilisateur veux faire
     switch ($choix) {
         case $choix1:
+            // Rappel de fonction pour ajouter des contacts
         $nom = readline("Entrez le prénom de la personne : ");
         $numero = readline("Entrez le numéro de téléphone : ");
         PushTabl($nom, $numero);
             break;
     
         case $choix2:
+            // Rappel de fonction pour rechercher un contact
         $nomRecherche = readline("Entrez le prénom de la personne à rechercher : ");
         $resultatRecherche = recherche($nomRecherche);
         echo "**********************************************************************\n";
@@ -74,17 +81,22 @@ $choix = readline("Que voulez-vous faire ? ");
             break;
     
         case $choix3:
+            // Rappel de fonction pour voir tout les contacts
         toutVoir();
             break;
     
         default:
+        // Si il fait un choix hors de ceux proposé
             echo "Merci de saisir un choix valide !";
             break;
     }
-
+    // Demande pour continuer ou sortir de la boucle
     $choix = readline("Voulez-vous faire autre chose dans votre menu ? ");
-} while ($choix == "Oui");
+    // Strtolower pour que même si il met en majuscule ca met en minuscule et valide la condition
+    $choix = strtolower($choix);
+} while ($choix == "oui");
 
+// Fin de programme et de la boucle
 echo "Merci au revoir";
 
 
